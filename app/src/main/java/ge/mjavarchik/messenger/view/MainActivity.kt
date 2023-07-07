e8cc0432f6b712f5de001b2c66a7d54d2ab7de80
MJava2002 <mjava20@freeuni.edu.ge>
2023-07-07 16:02:29 +0400

    sign_up_page ver.2 added checks maxLength of edittext and empty views when button is clicked

--- app/src/main/java/ge/mjavarchik/messenger/view/MainActivity.kt
+++ app/src/main/java/ge/mjavarchik/messenger/view/MainActivity.kt
@@ -1,12 +1,24 @@
 package ge.mjavarchik.messenger.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import ge.mjavarchik.messenger.R
+import ge.mjavarchik.messenger.databinding.ActivityMainBinding
+import ge.mjavarchik.messenger.databinding.LogInPageBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: LogInPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = LogInPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun signUP(view: View) {
+        startActivity(Intent(this, SignUpActivity::class.java))
     }
 }
---
+++ app/src/main/java/ge/mjavarchik/messenger/view/SignUpActivity.kt
@@ -0,0 +1,53 @@
+package ge.mjavarchik.messenger.view
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ge.mjavarchik.messenger.databinding.ActivityMainBinding
+import ge.mjavarchik.messenger.databinding.SignUpLayoutBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: SignUpLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SignUpLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val signUpButton: Button = binding.signUpButton
+        signUpButton.setOnClickListener {
+            val nickSignUp: EditText = binding.nickSignUp
+            val passwordSignUp: EditText = binding.passwordSignUp
+            val professionSignUp: EditText = binding.professionSignUp
+
+            val nickText: String = nickSignUp.text.toString()
+            val passwordText: String = passwordSignUp.text.toString()
+            val professionText: String = professionSignUp.text.toString()
+
+            if (nickText.isEmpty() || passwordText.isEmpty() || professionText.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                // to add input in firebase ra
+            }
+        }
+        val editText: EditText = binding.professionSignUp
+        val maxLength = 100
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null && s.length >= maxLength) {
+                    Toast.makeText(this@SignUpActivity, "Maximum number of symbols should be less than $maxLength", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+
+
+}
