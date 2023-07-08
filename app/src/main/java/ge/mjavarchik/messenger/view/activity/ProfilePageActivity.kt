fc8db17403ab5ab9539950471a829fa28ca399db
Rchik123 <rchik20@freeuni.edu.ge>
2023-07-08 22:06:31 +0400

    implement layout, update gitignore

--- app/src/main/java/ge/mjavarchik/messenger/view/activity/ProfilePageActivity.kt
+++ app/src/main/java/ge/mjavarchik/messenger/view/activity/ProfilePageActivity.kt
@@ -3,10 +3,15 @@ package ge.mjavarchik.messenger.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ge.mjavarchik.messenger.R
+import ge.mjavarchik.messenger.databinding.ActivityProfilePageBinding
 
 class ProfilePageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfilePageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_page)
+        binding = ActivityProfilePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
